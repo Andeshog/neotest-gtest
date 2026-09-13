e572dd02192124916fd154f11865641872a935ad
Anders Høgden <ahoegden@icloud.com>
2026-09-13 13:48:33 +0200

    remove comment blocks from TREESITTER_GTEST_QUERY

--- lua/neotest-gtest/parse.lua
+++ lua/neotest-gtest/parse.lua
@@ -34,11 +34,8 @@ local TREESITTER_GTEST_QUERY = vim.treesitter.query.parse(
           declarator: (identifier) @test.kind
         parameters: (
           parameter_list
-            . (comment)*
             . (parameter_declaration type: (type_identifier) !declarator) @namespace.name
-            . (comment)*
             . (parameter_declaration type: (type_identifier) !declarator) @test.name
-            . (comment)*
         )
       )
       !type
